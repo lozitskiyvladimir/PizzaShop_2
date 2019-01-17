e9c98636f399af20891090e01c89ca27288f0172
Lozitskiy Vladimir <loza-w@mail.ru>
2019-01-17 20:17:06 +0300

    update cart function

--- public/app.js
+++ public/app.js
@@ -15,3 +15,20 @@ function add_to_cart(id)
   x = x * 1 + 1;
   window.localStorage.setItem(key, x);
 }
+
+function cart_get_number_of_items()
+{
+  for (var i = 0; i < localStorage.length; i++)
+  {
+      var key = window.localStorage.key(i);
+      var value = window.localStorage.getItem(key);
+      if(key.indexOf('product_') == 0)
+      {
+        snt = snt + value * 1;
+      }
+    }
+      //return from method the string variable  "orders"
+      return snt;
+
+   }
+}
