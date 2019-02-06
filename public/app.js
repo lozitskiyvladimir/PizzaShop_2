2dab0617eb56e3415142dc967ac35bdabbf8b88b
Lozitskiy Vladimir <loza-w@mail.ru>
2019-02-06 23:46:04 +0300

    Add cart_get_orders

--- public/app.js
+++ public/app.js
@@ -33,6 +33,26 @@ function cart_get_number_of_items()
       //return from method the string variable  "orders"
       return cnt;
 
+}
 
+function cart_get_orders()
+{
+  //variable "orders" contain string like this
+  //"'product_1' = 'count', 'product_2' = 'count', 'product_3 ' = 'count' "
+  var orders = '';
+  // take each elements  localStorage and if a key begin with " product_"
+  // that  add it to  variable "orders"
+  for (var i = 0; i < localStorage.length; i++)
+  {
+      var key = window.localStorage.key(i);
+      var value = window.localStorage.getItem(key);
+      if(key.indexOf('product_') == 0)
+      {
+        orders = orders + key + '=' + value + ',';
+      }
+
+  }
+  //return from method the string variable  "orders"
+  return orders;
 
 }
