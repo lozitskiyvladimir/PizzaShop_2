832d4200d30f319857e3d1b7b063656019f9f2e3
Lozitskiy Vladimir <loza-w@mail.ru>
2019-02-07 21:27:01 +0300

    Add cart view

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ end
 post '/cart' do
 	orders_input = params[:orders]
 	@orders = parse_orders_input orders_input
-	erb "Hello! #{@orders.inspect}"
+	erb :cart
 end
 def parse_orders_input orders_input
     s1 = orders_input.split(/,/)
